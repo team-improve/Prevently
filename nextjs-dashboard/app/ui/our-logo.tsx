a84dc754f326bfe410efd748971d60016ad0e10a
DanielCristian10 <danielocristian@yahoo.com>
2025-10-22 14:56:08 +0300

    Name and logo

---
+++ nextjs-dashboard/app/ui/our-logo.tsx
@@ -0,0 +1,18 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+export default function OurLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Image
+        src="/finance_mode_90dp_FFFFFF.png"
+        width={500}
+        height={500}
+        alt=":D"
+      />
+      <p className="text-[44px]">Predictly</p>
+    </div>
+  );
+}
